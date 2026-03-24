7c747d7d9f802e90a76797833e270495753acbd4
Gyula <erdeigyula3@gmail.com>
2026-03-24 22:00:35 +0100

    a

--- src/main/java/net/farkas/crouch_cushion/CrouchCushion.java
+++ src/main/java/net/farkas/crouch_cushion/CrouchCushion.java
@@ -2,6 +2,7 @@ package net.farkas.crouch_cushion;
 
 import com.mojang.logging.LogUtils;
 
+import net.farkas.crouch_cushion.event.ModForgeEvents;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -14,7 +15,7 @@ public class CrouchCushion {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public CrouchCushion(FMLJavaModLoadingContext context) {
-        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(ModForgeEvents.class);
 
         context.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
     }
